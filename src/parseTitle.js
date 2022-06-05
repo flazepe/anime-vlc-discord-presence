a67463ef9a1d6606ce21a5dc8b03ae8fa7e3e5a8
Flaze <51487578+flazepe@users.noreply.github.com>
2022-06-05 11:04:20 +0800

    refactor: parseTitle function

--- src/parseTitle.js
+++ src/parseTitle.js
@@ -1,49 +1,48 @@
-function getTitleAndEpisode(details) {
-	let parts = [details];
+function matchTitleAndEpisode(details) {
+	// Episode 1, Ep.1, EP1, E1
+	let match = details.match(/(episode\s?|ep?\.?\s?|e)(\d+)/i);
+	if (!isNaN(match?.[2])) return [details.replace(match[0], ""), match[2]];
 
-	for (const part of parts) {
-		// Episode 1, Ep.1, EP1, E1
-		let match = part.match(/(episode\s?|ep?\.?\s?|e)(\d+)/i);
-		if (!isNaN(match?.[2])) return [part.replace(match[0], ""), match[2]];
+	// 5x3 (Season 5 Episode 3)
+	match = details.match(/\b\d+x(\d+)\b/i);
+	if (!isNaN(match?.[1])) return [details.replace(match[0], ""), match[1]];
 
-		// 5x3 (Season 5 Episode 3)
-		match = part.match(/\b\d+x(\d+)\b/i);
-		if (!isNaN(match?.[1])) return [part.replace(match[0], ""), match[1]];
+	// 1v3 (Episode 1 Season 3)
+	match = details.match(/\b(\d+)v\d+\b/i);
+	if (!isNaN(match?.[1])) return [details.replace(match[0], ""), match[1]];
 
-		// 1v3 (Episode 1 Season 3)
-		match = part.match(/\b(\d+)v\d+\b/i);
-		if (!isNaN(match?.[1])) return [part.replace(match[0], ""), match[1]];
+	// 1 (just the number, at the end)
+	match = details.match(/\b\d+$/);
+	if (!isNaN(match?.[0])) return [details.slice(0, -match[0].length), match[0]];
 
-		// 1 (just the number, at the end)
-		match = part.match(/\b\d+$/);
-		if (!isNaN(match?.[0])) return [part.replace(/\b\d+$/, ""), match[0]];
+	/* This is usually unreliable, let's not get desperate
+	// 1 (just the number, anywhere)
+	match = details.match(/\d+/);
+	if (!isNaN(match?.[0])) return [details.replace(match[0], ""), match[0]];
+	*/
 
-		/* This is usually unreliable, let's not get desperate
-		// 1 (just the number, anywhere)
-		match = part.match(/\d+/);
-		if (!isNaN(match?.[0])) return [part.replace(match[0], ""), match[0]];
-		*/
-	}
-
-	return [parts[0].trim(), ""];
+	return [details, ""];
 }
 
 module.exports = function (originalTitle) {
-	// Stupid HTML entities
-	originalTitle = originalTitle.trim().replace(/&amp;#\d{1,4};/g, _match => String.fromCharCode(_match.slice(6, -1)));
-
-	// Video extensions
+	// Omit video extensions
 	if (originalTitle.endsWith(".mkv") || originalTitle.endsWith(".mp4")) originalTitle = originalTitle.slice(0, -4);
 
-	// Brackets
-	if (originalTitle.includes("]")) originalTitle = originalTitle.replace(/[\s_.]*\[[^\]]*\][\s_.]*/g, "");
+	// Decode stupid HTML entities
+	originalTitle = originalTitle.trim().replace(/&amp;#\d{1,4};/g, _match => String.fromCharCode(_match.slice(6, -1)));
 
-	// Parentheses
-	if (originalTitle.includes(")")) originalTitle = originalTitle.replace(/ *\([^)]*\) */g, "");
+	// Remove brackets and parentheses
+	originalTitle = originalTitle.replace(/(\[|\()(.+?)(\]|\))/g, "");
 
-	// Delimiters
-	if (!originalTitle.includes(" ")) originalTitle = originalTitle.split(/[._]/).join(" ");
+	const [title, episode] = matchTitleAndEpisode(originalTitle.trim());
 
-	const [title, episode] = getTitleAndEpisode(originalTitle);
-	return { title, episode };
+	return {
+		title: title
+			// Trim "-" at the end for some reason
+			.replace(/\s*-\s*$/g, "")
+			// Delimiters + omit more than one space
+			.split(/\s+|[._]/)
+			.join(" "),
+		episode: Number(episode)
+	};
 };
