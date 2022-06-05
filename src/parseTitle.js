a1779f177c2c268b5c7b19e3e78702bee74d67df
Flaze <51487578+flazepe@users.noreply.github.com>
2022-06-05 10:27:35 +0800

    refactor: don't need that function

--- src/parseTitle.js
+++ src/parseTitle.js
@@ -1,27 +1,18 @@
-function validEp(episode) {
-	return episode && !isNaN(episode[1]);
-}
-
 function getEpisode(details) {
 	let parts = [details];
-
-	if (details.includes("-")) {
-		parts = parts.concat(details.slice(0, details.indexOf("-")).trim());
-		parts = parts.concat(details.slice(details.indexOf("-") + 1).trim());
-	}
 
 	for (const part of parts) {
 		// ep1, episode 1, e1
 		let match = part.match(/(episode\s?|ep|e)(\d+)/i);
-		if (match && !isNaN(match[2])) return [part.replace(match[0], ""), match[2]];
+		if (!isNaN(match?.[2])) return [part.replace(match[0], ""), match[2]];
 
 		// 5x3 (season 5 ep 3)
-		match = part.match(/\d+x(\d+)/i);
-		if (validEp(match)) return [part.replace(match[0], ""), match[1]];
+		match = part.match(/\b\d+x(\d+)\b/i);
+		if (!isNaN(match?.[1])) return [part.replace(match[0], ""), match[1]];
 
 		// 1v3 (episode 1 season 3)
-		match = part.match(/(\d+)v\d+/i);
-		if (validEp(match)) return [part.replace(match[0], ""), match[1]];
+		match = part.match(/\b(\d+)v\d+\b/i);
+		if (!isNaN(match?.[1])) return [part.replace(match[0], ""), match[1]];
 
 		// 1 (just the number, at the end)
 		match = part.match(/\b\d+$/);
@@ -29,7 +20,7 @@ function getEpisode(details) {
 
 		// 1 (just the number, anywhere)
 		match = part.match(/(\d+)/i);
-		if (validEp(match)) return [part.replace(match[0], ""), match[1]];
+		if (!isNaN(match?.[1])) return [part.replace(match[0], ""), match[1]];
 	}
 
 	return [parts[0].trim(), ""];
